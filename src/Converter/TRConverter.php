ac4d3831b9ecbbc53efb3bd0bbc0dd5ccdecc482
Kyle McGrogan <kyle.mcgrogan@dvinci.com>
2015-11-03 16:20:41 -0500

    Treat tables as outside-in

--- src/Converter/TRConverter.php
+++ src/Converter/TRConverter.php
@@ -2,11 +2,26 @@
 
 namespace Mcgrogan91\HTMLToMarkdown\Converter;
 
+use League\HTMLToMarkdown\Configuration;
+use League\HTMLToMarkdown\ConfigurationAwareInterface;
 use League\HTMLToMarkdown\ElementInterface;
 use League\HTMLToMarkdown\Converter\ConverterInterface;
 
-class TrConverter implements ConverterInterface
+class TrConverter implements ConverterInterface, ConfigurationAwareInterface
 {
+    /**
+     * @var Configuration
+     */
+    protected $config;
+
+    /**
+     * @param Configuration $config
+     */
+    public function setConfig(Configuration $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @param ElementInterface $element
      *
@@ -14,7 +29,11 @@ class TrConverter implements ConverterInterface
      */
     public function convert(ElementInterface $element)
     {
-        var_dump($element);die();
+        if ($this->config->getOption('strip_tags', false)) {
+            return "|" . $element->getValue();
+        }
+
+        return html_entity_decode($element->getChildrenAsString());
     }
 
     /**
--- src/HtmlConverter.php
+++ src/HtmlConverter.php
@@ -2,6 +2,7 @@
 
 namespace Mcgrogan91\HTMLToMarkdown;
 
+use League\HTMLToMarkdown\ElementInterface;
 use Mcgrogan91\HTMLToMarkdown\Converter\TableConverter;
 use Mcgrogan91\HTMLToMarkdown\Converter\TrConverter;
 
@@ -22,7 +23,40 @@ class HtmlConverter extends \League\HTMLToMarkdown\HtmlConverter
         parent::__construct($options);
 
         $this->environment->addConverter(new TableConverter());
-        $this->environment->addConverter(new TrConverter());
+        //$this->environment->addConverter(new TrConverter());
+    }
+
+    /**
+     * Convert Children
+     *
+     * Recursive function to drill into the DOM and convert each node into Markdown from the inside out.
+     *
+     * Finds children of each node and convert those to #text nodes containing their Markdown equivalent,
+     * starting with the innermost element and working up to the outermost element.
+     *
+     * @param ElementInterface $element
+     */
+    private function convertChildren(ElementInterface $element)
+    {
+        // Don't convert HTML code inside <code> and <pre> blocks to Markdown - that should stay as HTML
+        if ($element->isDescendantOf(array('pre', 'code'))) {
+            return;
+        }
+
+        // If the node has children, convert those to Markdown first
+        if ($element->getTagName() !== "table"   && $element->hasChildren()) {
+            foreach ($element->getChildren() as $child) {
+                $this->convertChildren($child);
+            }
+        }
+
+        // Now that child nodes have been converted, convert the original node
+        $markdown = $this->convertToMarkdown($element);
+
+        // Create a DOM text node containing the Markdown equivalent of the original node
+
+        // Replace the old $node e.g. '<h3>Title</h3>' with the new $markdown_node e.g. '### Title'
+        $element->setFinalMarkdown($markdown);
     }
 
 }
